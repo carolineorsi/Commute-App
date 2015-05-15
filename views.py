a76a490ba28a10806a44167fb26a1c12be1ef203
Caroline Orsi <caroline.orsi@gmail.com>
2015-05-14 20:55:27 -0700

    Added app configuration.

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 import transit
+import os
+
+SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', "abcdefg")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def index():
@@ -12,4 +16,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
